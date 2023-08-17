19910c30863a7a440db04a624f4f892122ca767a
eltigueeere <laloeltigueeere@gmail.com>
2023-08-17 16:27:42 -0600

    Feat: consumiendo la API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout/index'
 import Card from '../../Components/Card/index'
 
 function Home() {
 
+  const [items, setItems] = useState(null)
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
       Este es el Home
-      <Card></Card>
+      {
+        items?.map(item => (
+          <Card key={items.id} data={item} />
+        ))
+      }
     </Layout>
   )
 }
